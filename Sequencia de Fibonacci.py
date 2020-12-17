1e6e4707906bd30c993ab2af67c562ea9758a848
João Lucas de Souza Soares <63076080+joaolucas1337@users.noreply.github.com>
2020-12-17 17:49:30 -0300

    Add files via upload

---
+++ Sequencia de Fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(max):
+    a, b, contador = 0, 1, 0
+    while contador < max:
+        a, b = b, a + b
+        yield a
+        contador = contador + 1
+
+for n in fibonacci(100000):
+    print(n)
